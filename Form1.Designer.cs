2a841f4172febf5d746e99b3865c42c92388a78a
eliasurruti <eliasurruti99@hotmail.com>
2021-08-09 11:41:17 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btAddPage = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.btAddBt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btDeleteControl = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btSearch = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btDeletePage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btText = new System.Windows.Forms.Button();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btAddPage
+            // 
+            this.btAddPage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btAddPage.Location = new System.Drawing.Point(248, 528);
+            this.btAddPage.Name = "btAddPage";
+            this.btAddPage.Size = new System.Drawing.Size(96, 23);
+            this.btAddPage.TabIndex = 0;
+            this.btAddPage.Text = "Nueva pagina";
+            this.toolTip1.SetToolTip(this.btAddPage, "Nueva pagina");
+            this.btAddPage.UseVisualStyleBackColor = true;
+            this.btAddPage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(24, 80);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 408);
+            this.tabControl1.TabIndex = 1;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            this.tabControl1.Deselecting += new System.Windows.Forms.TabControlCancelEventHandler(this.tabControl1_Deselecting);
+            this.tabControl1.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.tabControl1_ControlAdded);
+            this.tabControl1.ControlRemoved += new System.Windows.Forms.ControlEventHandler(this.tabControl1_ControlRemoved);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 382);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 382);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(768, 382);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(768, 382);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // btAddBt
+            // 
+            this.btAddBt.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btAddBt.Location = new System.Drawing.Point(56, 528);
+            this.btAddBt.Name = "btAddBt";
+            this.btAddBt.Size = new System.Drawing.Size(112, 23);
+            this.btAddBt.TabIndex = 2;
+            this.btAddBt.Text = "Agregar nota";
+            this.toolTip1.SetToolTip(this.btAddBt, "Agregar");
+            this.btAddBt.UseVisualStyleBackColor = true;
+            this.btAddBt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuPopup;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(296, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(296, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // btDeleteControl
+            // 
+            this.btDeleteControl.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btDeleteControl.Location = new System.Drawing.Point(472, 528);
+            this.btDeleteControl.Name = "btDeleteControl";
+            this.btDeleteControl.Size = new System.Drawing.Size(96, 24);
+            this.btDeleteControl.TabIndex = 6;
+            this.btDeleteControl.Text = "Borrar Nota";
+            this.toolTip1.SetToolTip(this.btDeleteControl, "Borrar");
+            this.btDeleteControl.UseVisualStyleBackColor = true;
+            this.btDeleteControl.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.toolTip1.UseAnimation = false;
+            this.toolTip1.UseFading = false;
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // btSearch
+            // 
+            this.btSearch.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btSearch.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btSearch.BackgroundImage")));
+            this.btSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btSearch.Location = new System.Drawing.Point(592, 16);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(24, 24);
+            this.btSearch.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.btSearch, "Buscar pagina");
+            this.btSearch.UseVisualStyleBackColor = false;
+            this.btSearch.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(736, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Ayudas desactivadas";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btDeletePage
+            // 
+            this.btDeletePage.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btDeletePage.Location = new System.Drawing.Point(656, 520);
+            this.btDeletePage.Name = "btDeletePage";
+            this.btDeletePage.Size = new System.Drawing.Size(104, 40);
+            this.btDeletePage.TabIndex = 8;
+            this.btDeletePage.Text = "Borrar pagina seleccionada";
+            this.btDeletePage.UseVisualStyleBackColor = true;
+            this.btDeletePage.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(480, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(632, 568);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(248, 560);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(728, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(296, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 14;
+            // 
+            // btText
+            // 
+            this.btText.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btText.Location = new System.Drawing.Point(184, 8);
+            this.btText.Name = "btText";
+            this.btText.Size = new System.Drawing.Size(88, 40);
+            this.btText.TabIndex = 15;
+            this.btText.Text = "Cambiar texto";
+            this.btText.UseVisualStyleBackColor = true;
+            this.btText.Click += new System.EventHandler(this.btText_Click);
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txtTexto.Location = new System.Drawing.Point(24, 16);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(136, 20);
+            this.txtTexto.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(825, 597);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.btText);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btDeletePage);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btDeleteControl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btAddBt);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btAddPage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAddPage;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btAddBt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btDeleteControl;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btDeletePage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btText;
+        private System.Windows.Forms.TextBox txtTexto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int contador = 1;
+        int ayudas = 0;
+        TabPage paginas = new TabPage();
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            #region Tooltip
+            toolTip1.SetToolTip(tabPage1, tabControl1.SelectedTab.Text); //Propiedad SetToolTip
+            toolTip1.SetToolTip(tabPage2, tabControl1.SelectedTab.Text);
+
+            toolTip1.ToolTipTitle = "Hola"; //Propiedad ToolTipTitle
+            toolTip1.InitialDelay = 200; //Propiedad InitialDelay
+            toolTip1.ReshowDelay = 200;    //Propiedad ReshowDelay
+            
+           
+         
+            #endregion
+
+
+            #region TabPage
+
+            //Propiedad Text
+
+            tabPage1.Text = "Pagina 1";
+            tabPage2.Text = "Pagina 2";
+            tabPage3.Text = "Pagina 3";
+            tabPage4.Text = "Pagina 4";
+
+
+            //Propiedad TabCount
+
+            label7.Text = "Cantidad de Paginas: " + tabControl1.TabCount; 
+
+
+
+            //Propiedad BackColor
+            
+            tabPage2.BackColor = System.Drawing.Color.LemonChiffon;
+            tabPage3.BackColor = System.Drawing.Color.PowderBlue;
+            tabPage4.BackColor = System.Drawing.Color.Thistle;
+            #endregion
+        }
+        #region TabControl
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            tabControl1.SelectTab(Convert.ToInt32(textBox1.Text)); //Propiedad SelectTab
+            textBox1.Text = "";
+            
+        }
+        #endregion
+        #region TabControl
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TextBox Nota = new TextBox();
+          
+            Nota.Size = new Size(300, 500) ;
+            
+           
+            #region TabControl
+            
+            tabControl1.SelectedTab.Controls.Add(Nota); //Propiedad SelectedTab
+
+            #endregion
+
+            #region ToolTip
+            toolTip1.SetToolTip(Nota, "Boton vacio");
+            #endregion
+        }
+        private void btText_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab.Text = txtTexto.Text;
+            txtTexto.Text = "";
+            toolTip1.SetToolTip(tabPage1, tabControl1.SelectedTab.Text); //Propiedad SetToolTip
+            toolTip1.SetToolTip(tabPage2, tabControl1.SelectedTab.Text);
+        }
+        #region TabControl
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e) //EVENTO SelectedIndexChanged
+        {
+            label1.Text = "id " + Convert.ToString(tabControl1.SelectedIndex +1); //Propiedad SelectedIndex
+
+
+        }
+        #endregion
+   
+      
+        #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region ToolTip
+
+            toolTip1.SetToolTip(paginas, "Pagina vacia");
+
+            #endregion
+            #region TabControl
+            tabControl1.Controls.Add(paginas); //Propiedad Add
+            paginas.Text = "Pagina 1";
+            label7.Text = "Cantidad de Paginas: " + tabControl1.TabCount;
+            #endregion
+
+        }
+
+      
+        
+        #region TabControl
+        private void tabControl1_Deselecting(object sender, TabControlCancelEventArgs e) //EVENTO Deselecting
+        { 
+            label3.Text = "Cambio de pagina " + Convert.ToString(contador); 
+            contador++;
+        }
+        #endregion
+
+      
+      
+
+        #region TabControl
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab.Controls.Clear();   //Propiedad Clear
+            
+        }
+        #endregion
+
+        #region Tooltip
+        private void button4_Click(object sender, EventArgs e)
+        {
+            toolTip1.Active = false; //Propiedad Active
+        }
+        #endregion
+        #region TabControl
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab); //Propiedad Remove
+            
+        }
+        #endregion
+        #region TabControl
+        private void tabControl1_ControlRemoved(object sender, ControlEventArgs e) //Eventro ControlRemoved
+        {
+            label4.Text = "se ha borrado la pagina";
+        }
+        #endregion
+        #region TabControl
+        private void tabControl1_ControlAdded(object sender, ControlEventArgs e) //Evento ControlAdded
+        {
+            label5.Text = "se ha agregado la pagina";
+        }
+        #endregion
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            label5.Text = "";
+            label4.Text = "";
+        }
+        #region Tooltip
+        private void toolTip1_Popup(object sender, PopupEventArgs e) //Evento PopUp
+        {
+            ayudas++;
+            label6.Text = "ayudas totales " + Convert.ToString(ayudas);
+        }
+        #endregion
+      
+    }
+
+
+
+}
